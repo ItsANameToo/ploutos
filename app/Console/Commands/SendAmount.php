e1b3578ed0c72a89e28390d91ad6a4eda465084e
ItsANameToo <itsanametoo@outlook.com>
2018-09-28 20:31:17 +0200

    feat: send option

---
+++ app/Console/Commands/SendAmount.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Ark\Broadcaster;
+use App\Services\Ark\Signer;
+use Illuminate\Console\Command;
+
+class SendAmount extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ark:send {amount} {recipient} {--smartbridge=}';
+
+    /**
+     * Execute the console command.
+     *
+     * @param \App\Services\Ark\Signer      $broadcaster
+     * @param \App\Services\Ark\Broadcaster $signer
+     */
+    public function handle(Signer $signer, Broadcaster $broadcaster): void
+    {
+        $smartbridge = is_null($this->option('smartbridge')) ? '' : $this->option('smartbridge');
+        $transfer = $signer->sign(
+            $this->argument('recipient'),
+            $this->argument('amount') * ARKTOSHI,
+            $smartbridge
+        );
+
+        if ($transfer->verify()) {
+            config('delegate.broadcastType') === 'spread'
+            ? $broadcaster->spread($transfer->toArray())
+            : $broadcaster->broadcast($transfer->toArray());
+        } else {
+            $this->error('The signed transaction could not be verified.');
+        }
+    }
+}
