21cd4fda221a7155fa82c55ba6d4c1980eebc155
Brian Faust <hello@basecode.sh>
2020-02-11 19:26:12 +0000

    Apply fixes from StyleCI

--- app/Jobs/ProcessBlock.php
+++ app/Jobs/ProcessBlock.php
@@ -13,8 +13,10 @@ use Illuminate\Queue\SerializesModels;
 
 class ProcessBlock implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
     /**
      * The block instance.
      *
